6f08ea22182d0b2e4a37cba601665e6bc2e2957d
Misael dos Santos <misaelbdossantos10@gmail.com>
2020-03-19 01:51:49 -0300

    Updates

---
+++ precache-manifest.b5818a17a46428b28a3b975f27ec94b9.js
@@ -0,0 +1,18 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d683bbf1c80452c9740a542de5a2df8e",
+    "url": "/spotify-clone-react/index.html"
+  },
+  {
+    "revision": "4217f312ea90daefc91c",
+    "url": "/spotify-clone-react/static/js/2.17be716d.chunk.js"
+  },
+  {
+    "revision": "bd19c5641b8966547581",
+    "url": "/spotify-clone-react/static/js/main.da4211b5.chunk.js"
+  },
+  {
+    "revision": "d5afaa9595fecc5f7881",
+    "url": "/spotify-clone-react/static/js/runtime~main.d41b2d74.js"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/spotify-clone-react/precache-manifest.0c7e6551e908d2c23fb6011a04744a52.js"
+  "/spotify-clone-react/precache-manifest.b5818a17a46428b28a3b975f27ec94b9.js"
 );
 
 self.addEventListener('message', (event) => {
